be4e8dfef1b09af4294da0fdc2cb0c7371ceb74b
barak <hazizb@post.bgu.ac.il>
2024-03-30 18:35:20 +0300

    starting the trivia game

--- Client.py
+++ Client.py
@@ -1,52 +1,74 @@
+import os
 import socket
 import threading
 import struct
 import sys
-import os
-import random
 
-# Constants
 UDP_PORT = 13117
 MAGIC_COOKIE = 0xabcddcba
-SERVER_ADDRESS = ('<server_ip>', UDP_PORT)  # Replace <server_ip> with the actual server IP
-
 
 class TriviaClient:
     def __init__(self, name):
         self.name = name
-        # define UDP socket
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.udp_socket.bind(('0.0.0.0', UDP_PORT))  # Bind to any available port
+        self.udp_socket.bind(('0.0.0.0', UDP_PORT))
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.running = True
 
     def start(self):
-
-        print(f"Client- {self.name} started, listening for offer requests...")
+        print(f"Client {self.name} started, listening for offer requests...")
         self.listen_to_broadcast()
-
-
 
     def listen_to_broadcast(self):
         while True:
-            data, addr = self.udp_socket.recvfrom(1024)
-            magic_cookie, msg_type = struct.unpack("!Ib", data[:5])
-            if magic_cookie == MAGIC_COOKIE and msg_type == 0x2:
-                server_name = data[5:37].decode('utf-8').strip()
-                server_port = struct.unpack("!H", data[37:39])[0]
-                print(f"Received offer from {server_name} at address {addr[0]}, connecting...")
-                # connect to server over TCP port
-                self.connect_to_server((addr[0], server_port))
-                break
+            try:
+                data, addr = self.udp_socket.recvfrom(1024)
+                magic_cookie, msg_type = struct.unpack("!Ib", data[:5])
+                if magic_cookie == MAGIC_COOKIE and msg_type == 0x2:
+                    server_name = data[5:37].decode('utf-8').strip()
+                    server_port = struct.unpack("!H", data[37:39])[0]
+                    print(f"Received offer from {server_name} at address {addr[0]}, connecting...")
+                    self.connect_to_server((addr[0], server_port))
+                    break
+            except Exception as e:
+                print(f"Error while listening for offers: {e}")
 
     def connect_to_server(self, server_addr):
         try:
             self.tcp_socket.connect(server_addr)
             self.tcp_socket.sendall(f"{self.name}\n".encode('utf-8'))
-            # threading.Thread(target=self.receive_server_data).start()
-            # self.send_user_input()
+            threading.Thread(target=self.receive_server_data).start()
+
         except Exception as e:
             print(f"Error connecting to server: {e}")
             sys.exit(1)
+
+    def receive_server_data(self):
+        while self.running:
+            try:
+                data = self.tcp_socket.recv(1024).decode('utf-8')
+                if data:
+                    #print(data)  # Print the received message
+                    self.send_user_input()
+            except Exception as e:
+                print(f"Error receiving data from server: {e}")
+                break
+
+
+    def send_user_input(self):
+        while self.running:
+            try:
+                user_input = input()
+                self.tcp_socket.sendall(user_input.encode('utf-8') + b'\n')
+            except KeyboardInterrupt:
+                print("Exiting...")
+                self.running = False
+                self.tcp_socket.close()
+                os._exit(0)
+
+
+
+
 
--- Server.py
+++ Server.py
@@ -9,25 +9,32 @@ UDP_PORT = 13117
 TCP_PORT = 5555
 MAGIC_COOKIE = 0xabcddcba
 GAME_DURATION = 10  # in seconds
+TRUE_STATEMENTS = ["Python is a programming language.", "The sun rises in the east."]
+FALSE_STATEMENTS = ["Water boils at 100 degrees Fahrenheit.", "The Earth is flat."]
+
 
 class TriviaServer:
     def __init__(self):
         self.running = True
-        self.server_name = "AwesomeTriviaServer"
         self.clients = []
-
-        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.tcp_socket.bind(('0.0.0.0',TCP_PORT))
-        self.tcp_socket.listen(5)  # Listen for incoming connections
-
-    def start(self):
+        self.server_name = "AwesomeTriviaServer"
+        # To Make sure this field is always 32 characters long even if your server name is shorter.
+        self.padded_server_name = self.server_name.ljust(32)[:32]
         # define UDP socket
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.udp_socket.bind(('0.0.0.0', UDP_PORT))
+        # define TCP socket
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.tcp_socket.bind(('0.0.0.0', TCP_PORT))
+
+
+    def start(self):
+        self.tcp_socket.listen(5)  # Listen for incoming connections
         print(f"Server started, listening on IP address {self.udp_socket.getsockname()[0]}...")
+
         threading.Thread(target=self.broadcast_message).start()
         threading.Thread(target=self.wait_for_clients).start()
 
@@ -35,36 +42,66 @@ class TriviaServer:
         while True:
             # UDP Broadcast the message to all devices on the network
             broadcast_address = ('<broadcast>', UDP_PORT)
-            message = struct.pack("!Ib32sH", MAGIC_COOKIE, 0x2, self.server_name.encode('utf-8'), TCP_PORT)
+            message = struct.pack("!Ib32sH", MAGIC_COOKIE, 0x2, self.padded_server_name.encode('utf-8'), TCP_PORT)
             self.udp_socket.sendto(message, broadcast_address)
             # Sleep for a short duration to avoid flooding the network
             time.sleep(1)  # You can adjust the sleep duration as needed
 
     def wait_for_clients(self):
         start_time = time.time()
-        self.udp_socket.settimeout(GAME_DURATION)
-        self.tcp_socket.settimeout(1)
+        self.tcp_socket.settimeout(GAME_DURATION)
         while self.running:
             try:
-                data, addr = self.udp_socket.recvfrom(1024)
-                conn, _ = self.tcp_socket.accept()  # Accept TCP connection
-                threading.Thread(target=self.handle_tcp_client, args=(conn, addr)).start()
+                conn, tcp_addr = self.tcp_socket.accept()  # Accept TCP connection
+                threading.Thread(target=self.handle_tcp_client, args=(conn, tcp_addr)).start()
             except socket.timeout:
                 if time.time() - start_time >= GAME_DURATION:
                     self.running = False
-                    print("great!")
-                    #self.start_game()
-                    break
+                    self.start_game()
 
     def handle_tcp_client(self, conn, addr):
         team_name = conn.recv(1024).decode('utf-8').strip()
-        self.clients.append((team_name, addr))
-        print(f"Team {team_name} connected from {addr}")
-        conn.close()
+        self.clients.append((team_name, conn))  # Store client conn
+        print(f"Team {team_name} connected from {addr[0]}")
+
+    def start_game(self):
+
+        start_time = time.time()
+        true_statement = random.choice(TRUE_STATEMENTS)
+        false_statement = random.choice(FALSE_STATEMENTS)
+        true_false = (true_statement, false_statement)
+
+        # Build the welcome message
+        message = f"Welcome to the {self.server_name}, where we are answering trivia questions about NBA. \n"
+        counter = 1
+        for client in self.clients:
+            message += f"Player {counter} : {client[0]}\n"
+            counter += 1
+        message += f" == \n"
+        stat=random.choice(true_false)
+        message += f"True or False: {stat}\n"
+        print(message)
+        # Send the welcome message to all clients
+        for client in self.clients:
+            name, conn = client
+            try:
+                conn.sendall(f"{name}\n{message}".encode('utf-8'))  # Send the name and the message
+                threading.Thread(target=self.handle_client_answer, args=(conn,stat,name)).start()
+            except Exception as e:
+                print(f"Error sending data to client {name}: {e}")
 
 
+    def handle_client_answer(self, conn,stat,client_name):
+        try:
+            ans = conn.recv(1024).decode('utf-8').strip()  # Receive answer from client
+            # check if the answer is correct
+            if ((ans.lower() == "y" or ans.lower() == "t" or ans == "1")and stat in TRUE_STATEMENTS) or ((ans.lower() == "n" or ans.lower() == "f" or ans == "0")and stat in FALSE_STATEMENTS):
+                print(f"{client_name} is correct !")
+
+            else:
+                print(f"{client_name} is incorrect !")
 
 
-
-
+        except Exception as e:
+            print(f"Error while receiving answer from client: {e}")
 
